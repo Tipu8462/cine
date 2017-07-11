4b4339776cbf0f39bef3e7ad006d38f964d8c3ff
Abul Hasnat Nayeem <nayeem@softbdltd.com>
2017-07-12 01:36:45 +0600

    add slider

--- balaka/logobar.php
+++ balaka/logobar.php
@@ -1,10 +1,25 @@
 <div id="logo">
-	<div id="left_logo">
-    <img src="images/logo.png" width="99%" height="95%">
-    </div>
-    
-    <div id="right_logo">
-	<img src="images/cinema_hall.png" width="100%" height="95%">
-    </div>
+	<div class="w3-content w3-display-container">
+  <img class="mySlides" src="../images/coffee.jpg" style="width:100%">
+  <img class="mySlides" src="../images/sound.jpg" style="width:100%">
+  <img class="mySlides" src="../images/workbench.jpg" style="width:100%">
 </div>
+</div>
+<script>
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1} 
+    x[slideIndex-1].style.display = "block"; 
+    setTimeout(carousel, 3000); // Change image every 2 seconds
+}
+</script>
 
